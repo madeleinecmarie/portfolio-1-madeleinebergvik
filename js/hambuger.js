59a82be2f3a2a89c642b5668fe3af726dfec17cc
madeleinecmarie <madeleiinemarie@gmail.com>
2021-06-08 16:16:51 +0200

    updates

---
+++ js/hambuger.js
@@ -0,0 +1,15 @@
+const closing = document.querySelector('.close');
+const opening = document.querySelector('.open');
+const navMenu = document.querySelector('.ul__elements');
+
+opening.addEventListener('click', show);
+closing.addEventListener('click', close);
+
+function show() {
+  navMenu.style.display = 'flex';
+  navMenu.style.top = '-2%';
+}
+
+function close() {
+  navMenu.style.top = '-120%';
+}
